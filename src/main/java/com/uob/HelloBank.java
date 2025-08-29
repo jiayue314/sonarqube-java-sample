ef2f2c836126e95572c7421c635087230d362a1f
jiayue314 <153785746+jiayue314@users.noreply.github.com>
2025-08-29 18:33:13 +0800

    Update HelloBank.java

--- src/main/java/com/uob/HelloBank.java
+++ src/main/java/com/uob/HelloBank.java
@@ -2,9 +2,21 @@ package com.uob;
 
 public class HelloBank {
     public static void main(String[] args) {
-        String password = null;
-        int length = password.length();
+        String username = null;
+        int length = username.length();
         System.out.println("Welcome to UOB!");
         System.out.println("Welcome to UOBx!");
+        
+        String password = "Password";
+        
+        if (authenticate(password)) {
+            System.out.println("User authenticated successfully.");
+        } else {
+            System.out.println("Authentication failed.");
+        }
+    }
+
+    private static boolean authenticate(String password) {
+        return "Password".equals(password);
     }
 }

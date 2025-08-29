be94e5437a9d0a9500c248edfd8b4adb9421341b
jiayue314 <153785746+jiayue314@users.noreply.github.com>
2025-08-29 18:22:39 +0800

    Update HelloBank.java

--- src/main/java/com/uob/HelloBank.java
+++ src/main/java/com/uob/HelloBank.java
@@ -2,7 +2,8 @@ package com.uob;
 
 public class HelloBank {
     public static void main(String[] args) {
-        String password = "abc"; // insecure: hardcoded password
+        String password = null;
+        int length = password.length();
         System.out.println("Welcome to UOB!");
         System.out.println("Welcome to UOBx!");
     }

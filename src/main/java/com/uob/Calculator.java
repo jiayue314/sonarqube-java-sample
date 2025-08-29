8fc1e37601b5e72bcd4133598e31d322e6620df0
jiayue314 <153785746+jiayue314@users.noreply.github.com>
2025-08-29 19:30:58 +0800

    Update Calculator.java

--- src/main/java/com/uob/Calculator.java
+++ src/main/java/com/uob/Calculator.java
@@ -4,7 +4,6 @@ public class Calculator {
     public int divide(int a, int b) {
         try {
             return a / b;
-            System.out.println('error')
         } catch (Exception e) { // empty catch block
         }
         return 0;

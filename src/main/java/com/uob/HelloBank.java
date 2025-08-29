55b9f1e4b707e0212d6148af5a1c68bf013c7143
jiayue314 <153785746+jiayue314@users.noreply.github.com>
2025-08-29 19:47:22 +0800

    Update HelloBank.java

--- src/main/java/com/uob/HelloBank.java
+++ src/main/java/com/uob/HelloBank.java
@@ -2,9 +2,7 @@ package com.uob;
 
 public class HelloBank {
     public static void main(String[] args) {
-        String password = "abc";
-        int username = "username";
-        
+
         System.out.println("Welcome to UOB!");
         System.out.println("Welcome to UOBx!");
         

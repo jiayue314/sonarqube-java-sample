a06e1430a5a06afe21abeb38cfece40f482705b6
jiayue314 <153785746+jiayue314@users.noreply.github.com>
2025-08-29 17:01:32 +0800

    AddBug.java

--- src/main/java/com/uob/Calculator.java
+++ src/main/java/com/uob/Calculator.java
@@ -3,7 +3,8 @@ package com.uob;
 public class Calculator {
     public int divide(int a, int b) {
         try {
-            return a / 0;
+            return a / b;
+            System.out.println('error')
         } catch (Exception e) { // empty catch block
         }
         return 0;

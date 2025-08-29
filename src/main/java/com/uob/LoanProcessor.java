01c4787e6c7675dd07f99258aab18343381d2268
jiayue314 <153785746+jiayue314@users.noreply.github.com>
2025-08-29 19:47:53 +0800

    Update LoanProcessor.java

--- src/main/java/com/uob/LoanProcessor.java
+++ src/main/java/com/uob/LoanProcessor.java
@@ -2,7 +2,6 @@ package com.uob;
 
 public class LoanProcessor {
     public void processLoan() {
-        // TODO: add proper validation (Sonar will flag TODOs)
         System.out.println("Processing loan.x.");
     }
 }

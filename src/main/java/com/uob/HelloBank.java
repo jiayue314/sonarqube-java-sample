f4c8b6c58407aad02e63dd384b8d47cbaa058e62
jiayue314 <153785746+jiayue314@users.noreply.github.com>
2025-08-29 19:35:11 +0800

    Update HelloBank.java

--- src/main/java/com/uob/HelloBank.java
+++ src/main/java/com/uob/HelloBank.java
@@ -3,8 +3,7 @@ package com.uob;
 public class HelloBank {
     public static void main(String[] args) {
         String password = null;
-        int length = password.length();
-        System.out.println("Password length: " + length);
+        int length = password//2;
         
         System.out.println("Welcome to UOB!");
         System.out.println("Welcome to UOBx!");
